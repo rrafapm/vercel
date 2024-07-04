7d83ea344f3c8f880a086850dc9de50f015409f1
Rafael Peña <rrafapm@gmail.com>
2024-07-04 17:49:19 +0200

    little change

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const app =  express();
 const port = process.env.PORT || 3000;
 
 app.get('/api',(req, res) => {
-    res.send("<h1>Hola caracola</h1>");
+    res.send("<h1>Hola caracola! Como'tamos?</h1>");
 });
 
 app.listen(port, () =>{
